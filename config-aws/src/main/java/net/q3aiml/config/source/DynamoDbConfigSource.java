a219f7b89aed5f6d265531017745a7d85d4a9ef9
Andy Clayton <q3aiml@gmail.com>
2015-09-20 20:46:36 -0500

    small dynamodb source refactor

--- config-aws/src/main/java/net/q3aiml/config/source/DynamoDbConfigSource.java
+++ config-aws/src/main/java/net/q3aiml/config/source/DynamoDbConfigSource.java
@@ -7,6 +7,7 @@ import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.google.common.collect.ImmutableMap;
 import net.q3aiml.config.PollResult;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,22 +32,26 @@ public class DynamoDbConfigSource implements PollingConfigSource {
             ScanRequest scanRequest = new ScanRequest(tableName)
                     .withExclusiveStartKey(lastEvaluatedKey);
             ScanResult scanResult = dynamo.scan(scanRequest);
-            for (Map<String, AttributeValue> item : scanResult.getItems()) {
-                AttributeValue keyAttribute = item.get(keyAttributeName);
-                AttributeValue valueAttribute = item.get(valueAttributeName);
-                if (keyAttribute == null) {
-                    throw new IllegalArgumentException("expected " + tableName + " item " + item
-                            + " to have " + keyAttributeName + " attribute");
-                }
-                if (valueAttribute == null) {
-                    throw new IllegalArgumentException("expected " + tableName + " item " + item
-                            + " to have " + valueAttributeName + " attribute");
-                }
-                results.put(keyAttribute.getS(), valueAttribute.getS());
-            }
+            handleItems(scanResult.getItems(), results);
             lastEvaluatedKey = scanResult.getLastEvaluatedKey();
         } while (lastEvaluatedKey != null && !lastEvaluatedKey.isEmpty());
         return PollResult.fullUpdate(results.build());
+    }
+
+    protected void handleItems(List<Map<String, AttributeValue>> items, ImmutableMap.Builder<String, String> results) {
+        for (Map<String, AttributeValue> item : items) {
+            AttributeValue keyAttribute = item.get(keyAttributeName);
+            AttributeValue valueAttribute = item.get(valueAttributeName);
+            if (keyAttribute == null) {
+                throw new IllegalArgumentException("expected " + tableName + " item " + item
+                        + " to have " + keyAttributeName + " attribute");
+            }
+            if (valueAttribute == null) {
+                throw new IllegalArgumentException("expected " + tableName + " item " + item
+                        + " to have " + valueAttributeName + " attribute");
+            }
+            results.put(keyAttribute.getS(), valueAttribute.getS());
+        }
     }
 
     @Override
